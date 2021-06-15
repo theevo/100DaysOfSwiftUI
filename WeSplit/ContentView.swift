3272f8d105e9167db4aaf09f225e0bbbf953fbd6
Theo Vora <theo.vora@gmail.com>
2021-06-15 13:54:19 -0600

    Complete day 16. Examples of @State and 2-way bindings.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let bears = ["Baby Bear", "Momma Bear", "Pappa Bear"]
+    let bearEmojis = ["🧸", "🐻‍❄️" , "🐻" ]
+    @State private var selectedBear = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Picker("Select your student", selection: $selectedBear) {
+            ForEach(0 ..< bears.count) {
+                Text(self.bears[$0])
+            }
+        }
+        Text("You selected \(bearEmojis[selectedBear])").font(.title)
     }
+    
+//    @State private var name = ""
+//
+//    var body: some View {
+//        Form {
+//            TextField("Enter your name", text: $name)
+//            Text("Your name is \(name)")
+//        }
+//    }
+    
+//    @State private var tapCount = 0
+//
+//    var body: some View {
+//        Button("Tap Count \(tapCount)") {
+//            self.tapCount += 1
+//        }
+//    }
+//    var body: some View {
+//        NavigationView {
+//            Form {
+//                Section {
+//                    Text("Hello, world!")
+//                    //            .padding()
+//                }
+//            }
+//            .navigationBarTitle("SwiftUI")
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
